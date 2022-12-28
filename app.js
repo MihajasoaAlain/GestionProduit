125d8d701482e7c2081cb8368a103e7808b3b5fc
Mihajasoa <mihajasoaalain85@gmail.com>
2022-12-28 14:47:17 -0800

    version singlePage fait

--- app.js
+++ app.js
@@ -1,45 +1,73 @@
-const homeMenu = document.querySelectorAll('.box');
-const contenuHome = document.querySelector('.home');
-const ajout = document.querySelector('.ajout');
-const acceuil = document.querySelector('#acceuil');
-const AddItems = document.querySelector('.imageAdd');
-let compteur =0;
-let data={};
-acceuil.addEventListener('click',function(){changePage(ajout,contenuHome)});
-homeMenu.forEach(menu =>{
-    menu.addEventListener('click',function(){changePage(contenuHome,ajout)})
+const onglets = document.querySelectorAll('.onglet');
+const contenu = document.querySelectorAll('.contenu');
+let index =0;
+onglets.forEach(onglet =>{
+
+    onglet.addEventListener('click',()=>
+    {
+        if (onglet.classList.contains('active')){
+        return;
+         }
+        else{
+        onglet.classList.add('active');
+        }
+        index = onglet.getAttribute('data-anim')
+        for (i=0 ; i < onglets.length ; i++){
+            if(onglets[i].getAttribute('data-anim')!=index){
+                onglets[i].classList.remove('active');
+            }
+        }
+        for (j = 0;j <contenu.length;j++){
+            if (contenu[j].getAttribute('data-anim')==index){
+                contenu[j].classList.add('activeContenu');
+                contenu[j].classList.remove('hide')
+            }
+            else{
+                contenu[j].classList.remove('activeContenu');
+                contenu[j].classList.add('hide');
+            }
+        }
+    })
 });
-function changePage(initial,final){
-    if(final.classList.contains('hide')){
-        initial.classList.remove('activeContenu');
-        final.classList.remove('hide');
-        final.classList.add('activeContenu');
-        initial.classList.add('hide');
+// Ajout de nouveau produit
+const produit = {};
+
+const bouton = document.querySelector('.imageAdd');
+const boxProduit = document.querySelector('#productList');
+
+bouton.addEventListener('click',()=>{
+    const nom = document.querySelector('#nom');
+    const identif = document.querySelector('#numeroId');
+    const stock = document.querySelector('#stock');
+        const product ={
+            productNom : nom,
+            productID :identif,
+            productStock : stock
+        };
+        localStorage.setItem(product.productNom,JSON.stringify(product));
+            for (let i = 0; i<localStorage.length;i++){
+                const key =localStorage.key(i);
+                const item = JSON.parse(localStorage.getItem(key));
+                creation(item);
+            }
+});
+    function creation(item){
+        const row = document.createAttribute('tr');
+        const name = document.createElement('td');
+        const identification = document.createElement('td');
+        const stock = document.createElement('td');
+        const suppr = document.createElement('td');
+        const ajoutP = document.createElement('td');
+        name.textContent = item.productNom;
+        identification.textContent= item.productID;
+        stock.textContent = item.productStock;
+        // name.appendChild(row);
+        // identification.appendChild(row);
+        // stock.appendChild(row);
+        // suppr.appendChild(row);
+        // ajoutP.appendChild(row);
+        // row.appendChild(boxProduit);
     }
-   
-}
-            // pour faire un petite stockage offline
 
-AddItems.addEventListener('click',()=>{
 
-    const DataNom = document.querySelector('#nom').value;
-    const DataNumID = document.querySelector('#numeroId').value;
-    const DataStock = document.querySelector('#stock').value;
-    const produit ={
-        Nom : DataNom ,
-        numeroId : DataNumID,
-        stock : DataStock
-    };
-
-    const produit1 =JSON.stringify(produit);
-    compteur++;
-    localStorage.setItem("nom["+compteur+"]",produit1);
-    data = localStorage.getItem("nom["+compteur+"]");
-console.log(data);
-});
-
-for(let i = 0 ; i<compteur;i++){
-    data = localStorage.getItem("nom["+i+"]");
-    console.log(data);
-}
 
